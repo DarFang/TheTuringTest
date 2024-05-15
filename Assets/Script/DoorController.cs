196b44e0dc48a21a794d5500ccde1cda1d0cceb8
Darius Fang <dariusfang1@gmail.com>
2024-05-14 22:38:42 -0600

    added interactable

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    private void OnTriggerEnter(Collider other) {
+    public void OpenDoor()
+    {
         animator.SetBool("DoorOpen", true);
     }
-    private void OnTriggerExit(Collider other) {
+    public void CloseDoor()
+    {
         animator.SetBool("DoorOpen", false);
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        // animator.SetBool("DoorOpen", true);
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        // animator.SetBool("DoorOpen", false);
     }
 }
--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -6,21 +6,29 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] Camera camera;
-    [SerializeField] private float speed = 2f; 
-    [SerializeField] private float MouseSensitiviy = 50f;
-    [SerializeField] private Transform shootingPoint;
+    [Header("Movement")]
     [SerializeField] private float speedMultiplier = 1.5f; 
     [SerializeField] private float JumpForce = 10f;
     [SerializeField] private float MaxFallSpeed = -10f;
     [SerializeField] private LayerMask floorLayer;
+    [SerializeField] private float speed = 2f; 
+    [SerializeField] private float MouseSensitivity = 50f;
+    [SerializeField] Vector3 velocity;
+
+    [Header("Aiming")]
     [SerializeField] private Rigidbody projectile;
+    [SerializeField] private Transform shootingPoint;
+    [SerializeField] Camera camera;
+
+    [Header("Interactions")]
+    [SerializeField] private LayerMask interactableLayer;
+
     CharacterController controller;
     private Vector2 aimDirection; 
     private Vector2 moveDirection;
-    [SerializeField] Vector3 velocity;
-    const float gravityAceleration = -9.81f;
+    const float gravityAcceleration = -9.81f;
     bool canLookWithMouse;
+    private IInteractable targetInteractable;
 
     void Start()
     {
@@ -36,8 +44,30 @@ public class PlayerInput : MonoBehaviour
         Shoot();
         Jump();
         ApplyGravity();
-        //jump
-        //check if grounded
+        Interact();
+    }
+    void Interact()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2,0));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 4f, interactableLayer )){
+            if(targetInteractable == null){
+                targetInteractable = hit.collider.GetComponent<IInteractable>();
+                targetInteractable.OnHoverEnter();
+            }
+            else{
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    targetInteractable.Oninteract();
+                    // Debug.DrawRay(ray.origin, ray.direction, Color.red, 3f );
+                }
+            }
+        }
+        else if(targetInteractable != null)
+        {
+            targetInteractable.OnHoverExit();
+            targetInteractable = null;
+        }
     }
     void Shoot()
     {
@@ -58,7 +88,7 @@ public class PlayerInput : MonoBehaviour
     void ApplyGravity()
     {
         if(!IsGrounded()){
-            velocity.y += gravityAceleration *  Time.deltaTime;
+            velocity.y += gravityAcceleration *  Time.deltaTime;
             if(velocity.y <MaxFallSpeed){
                 velocity.y = MaxFallSpeed;
             }
@@ -84,13 +114,13 @@ public class PlayerInput : MonoBehaviour
     private void RotatePlayer()
     {
         aimDirection.x = Input.GetAxisRaw("Mouse X");
-        aimDirection.y += -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * MouseSensitiviy;
+        aimDirection.y += -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * MouseSensitivity;
 
         // Rotate player up down
         aimDirection.y = Mathf.Clamp(aimDirection.y, -85f, 85f);
         camera.transform.localEulerAngles = new Vector3(aimDirection.y, 0, 0);
         //Rotate player on left right
-        transform.Rotate(Vector3.up, aimDirection.x * Time.deltaTime * MouseSensitiviy);
+        transform.Rotate(Vector3.up, aimDirection.x * Time.deltaTime * MouseSensitivity);
     }
     bool IsGrounded()
     {
